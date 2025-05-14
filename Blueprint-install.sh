ba57b305caf412f2d7fddb2c7edbf0dd3fc4dbaa
Alfha240 <118652216+Alfha240@users.noreply.github.com>
2025-05-14 16:54:41 +0530

    Create Blueprint-install.sh

---
+++ Blueprint-install.sh
@@ -0,0 +1,20 @@
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt-get install -y nodejs
+
+
+npm i -g yarn
+cd /path/to/pterodactyl
+yarn
+apt install -y zip unzip git curl wget
+#now install plueprint zip
+wget "$(curl -s https://api.github.com/repos/BlueprintFramework/framework/releases/latest | grep 'browser_download_url' | cut -d '"' -f 4)" -O release.zip
+mv release.zip /path/to/pterodactyl/release.zip
+cd /path/to/pterodactyl
+unzip release.zip
+
+chmod +x blueprint.sh
+bash blueprint.sh
