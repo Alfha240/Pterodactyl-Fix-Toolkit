8e6813cd46c724021ae54011d78c12ecf3786466
Alfha240 <118652216+Alfha240@users.noreply.github.com>
2025-05-15 19:14:54 +0530

    Update Blueprint-install.sh

--- Blueprint-install.sh
+++ Blueprint-install.sh
@@ -1,14 +1,14 @@
 curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt install -y nodejs
 npm i -g yarn
-cd /path/to/pterodactyl
+cd /var/www/pterodactyl
 yarn install --network-timeout 600000
 apt update && apt upgrade -y
 apt install -y zip unzip git curl wget
 #now install plueprint zip
 wget "$(curl -s https://api.github.com/repos/BlueprintFramework/framework/releases/latest | grep 'browser_download_url' | cut -d '"' -f 4)" -O release.zip
-mv release.zip /path/to/pterodactyl/release.zip
-cd /path/to/pterodactyl
+mv release.zip /var/www/pterodactyl/release.zip
+cd /var/www/pterodactyl
 unzip release.zip
 touch /var/www/pterodactyl/.blueprintrc
 echo \
