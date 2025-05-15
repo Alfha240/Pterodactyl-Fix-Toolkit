131bdd1d3278f72fd15dd0e1874593138f5d4a2b
Alfha240 <118652216+Alfha240@users.noreply.github.com>
2025-05-15 19:11:57 +0530

    Update Blueprint-install.sh

--- Blueprint-install.sh
+++ Blueprint-install.sh
@@ -1,14 +1,9 @@
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-apt-get update
-apt-get install -y nodejs
-
-
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
+apt install -y nodejs
 npm i -g yarn
 cd /path/to/pterodactyl
-yarn
+yarn install --network-timeout 600000
+apt update && apt upgrade -y
 apt install -y zip unzip git curl wget
 #now install plueprint zip
 wget "$(curl -s https://api.github.com/repos/BlueprintFramework/framework/releases/latest | grep 'browser_download_url' | cut -d '"' -f 4)" -O release.zip
